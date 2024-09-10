5c4d4ccf7d79f1ccbc450969b9793f2726a47954
Noe L <geo.lanc63@gmail.com>
2024-09-09 19:38:27 -0600

    refactor: loading RDB file

--- app/rdbreader.go
+++ app/rdbreader.go
@@ -33,7 +33,7 @@ func LoadFile(path string) error {
 	reader := bufio.NewReader(hexReader)
 	metadataByte, _ := RDBHexStringToByte(RDB_METADATA_START)
 	dbSubsectionByte, _ := RDBHexStringToByte(RDB_DB_SUBSECTION_START)
-	hashTableByte, _ := RDBHexStringToByte(RDB_HASH_TABLE_START)
+	// hashTableByte, _ := RDBHexStringToByte(RDB_HASH_TABLE_START)
 	endOfFileByte, _ := RDBHexStringToByte(RDB_END_OF_FILE)
 
 	magicStringBytes, err := reader.ReadBytes(metadataByte)
@@ -45,7 +45,8 @@ func LoadFile(path string) error {
 		return err
 	}
 
-	fmt.Println("header:\n", string(magicStringBytes))
+	// TODO: check whether or not the RDB file is empty
+	bytesUntilEOF, err := reader.ReadBytes(endOfFileByte)
 
 	metadataSectionBytes, err := reader.ReadBytes(dbSubsectionByte)
 	if err != nil {
@@ -77,51 +78,20 @@ func LoadFile(path string) error {
 			if len(sectionBytes) == 0 {
 				break
 			}
-
-			fmt.Println("section bytes:", sectionBytes)
-
-			var value interface{}
-			attrType, bitRange := decodeBytes(sectionBytes[0])
-			if attrType == "" {
-				return fmt.Errorf("failed to decode byte %v", sectionBytes[0])
+			value, bitRange, err := decodeNextValue(sectionBytes)
+			if err != nil {
+				return err
 			}
-
-			fmt.Println("next type: ", attrType)
 			ignoreBits, useBits := bitRange[0], bitRange[1]
-			useBytes := (ignoreBits + useBits) / 8                          // calculate total bytes for read
-			binaryBitString := bytesToBinaryString(sectionBytes[:useBytes]) // read up to nth index byte from metadata into binarym
-			sizeBinaryBits := binaryBitString[ignoreBits:]                  // take the bits that represent the size
-			fmt.Println("ignore and use bits:", ignoreBits, useBits, sizeBinaryBits)
+			useBytes := (ignoreBits + useBits) / 8 // calculate total bytes for read
 
-			if attrType == "string" {
-				fmt.Println("string size bits:", len(sizeBinaryBits))
-				stringLength, err := strconv.ParseInt(sizeBinaryBits, 2, useBits)
-				if err != nil {
-					fmt.Printf("error parsing size binary string %s: %v\n", sizeBinaryBits, err)
-					break
-				}
-				fmt.Println("string byte length", stringLength)
-				value = string(sectionBytes[useBytes : stringLength+1])
-				sectionBytes = sectionBytes[stringLength+1:]
-				fmt.Println("string value: ", value)
-
-			}
-
-			if attrType == "int" {
-				if useBits > 8 {
-					sizeBinaryBitSlice := strings.Split(sizeBinaryBits, "") // TODO: check how to reverse an actual string
-					slices.Reverse(sizeBinaryBitSlice)
-					sizeBinaryBits = strings.Join(sizeBinaryBitSlice, "")
-				}
-				fmt.Println("integer value bytes: ", sizeBinaryBits)
-				value, err = binary.ReadVarint(bytes.NewReader([]byte(sizeBinaryBits)))
-				if err != nil {
-					fmt.Println("failed to decode int bytes", err)
-					break
-				}
-
+			switch v := value.(type) {
+			case int:
 				sectionBytes = sectionBytes[useBytes:]
-				fmt.Println("int value: ", value)
+			case string:
+				sectionBytes = sectionBytes[useBytes+len(v):]
+			default:
+				break
 			}
 
 			if nextInsert == "key" {
@@ -142,7 +112,6 @@ func LoadFile(path string) error {
 	if err != nil {
 		return err
 	}
-
 	// empty rdb file
 	if dbIndex[0] == 0xC0 {
 		_, err := reader.ReadBytes(endOfFileByte)
@@ -151,6 +120,7 @@ func LoadFile(path string) error {
 		}
 		bytesRead, _ := reader.Read(fileHash)
 		fmt.Println("Finished reading file. Hash is: ", fileHash[:bytesRead])
+		return nil
 	}
 
 	// non-empty rdb file
@@ -192,6 +162,53 @@ func decodeInt(input []byte, bitRange [2]int) (int, error) {
 	return int(integer), nil
 }
 
+// decodeBytes returns the RDBType and an 2-item array with the exact bits to ignore and use.
+// If there are no matches, ti returns an empty string and [2]int{0,0}
+func decodeNextValue(inputBytes []byte) (interface{}, [2]int, error) {
+	var err error
+	var decodedValue interface{}
+	var bitRange [2]int = [2]int{0, 0}
+	startByte := inputBytes[0]
+
+	switch {
+	case startByte <= 0b00111111:
+		bitRange = [2]int{2, 6}
+		decodedValue, err = decodeString(inputBytes, bitRange) // item 1: ignore bits, item 2: transform bits, (item1 + item2) / 8: advance bits
+	case startByte <= 0b01111111:
+		bitRange = [2]int{2, 14}
+		decodedValue, err = decodeString(inputBytes, bitRange)
+	case startByte <= 0b10111111:
+		bitRange = [2]int{8, 32} // ignore 6 remaining bits of the input size
+		decodedValue, err = decodeString(inputBytes, bitRange)
+	/* case startByte <= 0b11111111:
+	return decodeString(startByte) */
+	case startByte == 0xC0:
+		bitRange = [2]int{8, 8}
+		decodedValue, err = decodeInt(inputBytes, bitRange)
+	case startByte == 0xC1:
+		bitRange = [2]int{8, 16}
+		decodedValue, err = decodeInt(inputBytes, bitRange)
+	case startByte == 0xC2:
+		bitRange = [2]int{8, 32}
+		decodedValue, err = decodeInt(inputBytes, bitRange)
+	default:
+		err = fmt.Errorf("unable to decode byte %d", startByte)
+	}
+
+	if err != nil {
+		return decodedValue, bitRange, err
+	}
+	return decodedValue, bitRange, nil
+}
+
+func bytesToBinaryString(inputBytes []byte) string {
+	var builder strings.Builder
+	for _, b := range inputBytes {
+		builder.WriteString(fmt.Sprintf("%08b", b))
+	}
+	return builder.String()
+}
+
 func RDBHexStringToByte(hexString string) (byte, error) {
 	bytes, err := hex.DecodeString(hexString)
 
@@ -204,64 +221,6 @@ func RDBHexStringToByte(hexString string) (byte, error) {
 	}
 
 	return bytes[0], nil
-}
-
-// decodeBytes returns the RDBType and an 2-item array with the exact bits to ignore and use.
-// If there are no matches, ti returns an empty string and [2]int{0,0}
-func decodeBytes[T RDBValue](inputBytes []byte) (T, error) {
-	var err error
-	var valueType string
-	var decodedValue T
-	var bitRange [2]int = [2]int{0, 0}
-	startByte := inputBytes[0]
-
-	switch {
-	case startByte <= 0b00111111:
-		valueType = "string"
-		bitRange = [2]int{2, 6}
-	case startByte <= 0b01111111:
-		valueType = "string"
-		bitRange = [2]int{2, 14}
-	case startByte <= 0b10111111:
-		valueType = "string"
-		bitRange = [2]int{8, 32} // ignore 6 remaining bits of the input size
-	/* case startByte <= 0b11111111:
-	return decodeString(startByte) */
-	case startByte == 0xC0:
-		valueType = "int"
-		bitRange = [2]int{8, 8}
-	case startByte == 0xC1:
-		valueType = "int"
-		bitRange = [2]int{8, 16}
-	case startByte == 0xC2:
-		valueType = "int"
-		bitRange = [2]int{8, 32}
-	default:
-		valueType = ""
-	}
-
-	if valueType == "" {
-		return nil, fmt.Errorf("failed to decode byte %v", startByte)
-
-	}
-
-	if valueType == "string" {
-		decodedValue, err = decodeString(inputBytes, bitRange) // item 1: ignore bits, item 2: transform bits, (item1 + item2) / 8: advance bits
-	}
-
-	if err != nil {
-		return nil, err
-	}
-
-	return decodedValue, err
-}
-
-func bytesToBinaryString(inputBytes []byte) string {
-	var builder strings.Builder
-	for _, b := range inputBytes {
-		builder.WriteString(fmt.Sprintf("%08b", b))
-	}
-	return builder.String()
 }
 
 func GetRDBKeys(filePath, key string) (string, error) {
